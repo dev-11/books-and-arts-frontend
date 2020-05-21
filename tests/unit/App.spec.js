5b60f9724d01bd2c9c0e64f45cc8d957dc6eb8c4
Otto <otto@masterbranch.io>
2020-05-21 12:09:10 +0100

    testing api error

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -77,6 +77,27 @@ describe('App.vue', () => {
 
         expect(wrapper.vm.loading).toBeFalsy();
         expect(wrapper.vm.errored).toBeFalsy();
+
+        expect(wrapper.vm.services[0].family).toBe("test_family");
+        expect(wrapper.vm.services[0].service).toBe("test_service");
+        expect(wrapper.vm.services[0].full_name).toBe("test_family.test_service");
+        expect(wrapper.vm.services[0].service_type).toBe("books");
+        expect(wrapper.vm.services[0].data[0].section).toBe("test_section");
+        expect(wrapper.vm.services[0].data[0].books[0].id).toBe("95dc3a9ac68444a4b90f3e85aec3db9d");
+        expect(wrapper.vm.services[0].data[0].books[0].title).toBe("test_title");
+        expect(wrapper.vm.services[0].data[0].books[0].authors).toBe("test_authors");
+        expect(wrapper.vm.services[0].data[0].books[0].price).toBe("test_price");
+        expect(wrapper.vm.services[0].data[0].books[0].format).toBe("test_format");
+        expect(wrapper.vm.services[0].data[0].books[0].desc).toBe("test_desc");
+        expect(wrapper.vm.services[0].data[0].books[0].img).toBe("test_img");
+        expect(wrapper.vm.services[0].data[0].books[0].genres[0]).toBe("test_genre_1");
+        expect(wrapper.vm.services[0].data[0].books[0].genres[1]).toBe("test_genre_2");
+        expect(wrapper.vm.services[0].data[0].books[0].number_of_pages).toBe("352");
+        expect(wrapper.vm.services[0].data[0].books[0].published_at).toBe("30/04/2020");
+        expect(wrapper.vm.services[0].data[0].books[0].isbn).toBe("test_isbn");
+        expect(wrapper.vm.services[0].data[0].books[0].rating.average_rating).toBe("3.91");
+        expect(wrapper.vm.services[0].data[0].books[0].rating.work_ratings_count).toBe(603);
+
         expect(wrapper.vm.services[1].family).toBe("test_art_family");
         expect(wrapper.vm.services[1].service).toBe("test_art_service");
         expect(wrapper.vm.services[1].full_name).toBe("test_art_family.test_art_service");
@@ -88,5 +109,16 @@ describe('App.vue', () => {
         expect(wrapper.vm.services[1].data.exhibitions[0].date).toBe("test_date");
         expect(wrapper.vm.services[1].data.exhibitions[0].description).toBe("test_desc");
         expect(wrapper.vm.services[1].data.exhibitions[0].img).toBe("test_art_img");
+    });
+
+    it("api return error", async () => {
+        const mockAxios = new MockAdapter(axios);
+
+        mockAxios.onGet().reply(500)
+
+        const wrapper = shallowMount(App);
+        await flushPromises();
+        expect(wrapper.vm.loading).toBeFalsy();
+        expect(wrapper.vm.errored).toBeTruthy();
     });
 });
