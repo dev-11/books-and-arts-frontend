91d7cf71289a6472357321095a2e48d2ea429e4d
Otto <otto@masterbranch.io>
2020-05-22 10:37:44 +0100

    introducing smart rating count

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -13,7 +13,7 @@
             {{rating}}
         </div>
         <div class="ml-2 text-muted" style="float: left;">
-            ({{ratings_count.toLocaleString()}})
+            ({{smart_number}})
         </div>
     </div>
 </template>
@@ -34,6 +34,15 @@ export default {
     },
     empty_stars: function(){
         return Math.floor(5-(+this.rating));
+    },
+    smart_number: function() {
+      var num = this.ratings_count;
+        var b = (num).toPrecision(2).split("e"); // get power
+        var k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3); // floor at decimals, ceiling at trillions
+        var c = k < 1 ? num.toFixed(0) : (num / Math.pow(10, k * 3) ).toFixed(1); // divide by power
+        var d = c < 0 ? c : Math.abs(c); // enforce -0 is 0
+        var e = d + ['', 'k', 'm', 'B', 'T'][k]; // append power
+        return e;
     }
   }
 };
--- tests/unit/components/Rating.spec.js
+++ tests/unit/components/Rating.spec.js
@@ -95,3 +95,61 @@ describe("Rating.vue", () => {
     expect(wrapper.vm.empty_stars).toBe(0);
   });
 });
+
+// tests for smart rating count
+describe("Rating.vue", () => {
+  const wrapper = shallowMount(Rating, {
+    propsData: {
+      rating: "5.0",
+      ratings_count: 123456
+    },
+    stubs: ["b-icon-star-fill", "b-icon-star-half", "b-icon-star"]
+  });
+
+  it("tests for rating smart_count = 123456", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.vm.ratings_count).toBe(123456);
+    expect(wrapper.vm.full_stars).toBe(5);
+    expect(wrapper.vm.has_half_star).toBe(0);
+    expect(wrapper.vm.empty_stars).toBe(0);
+    expect(wrapper.vm.smart_number).toBe('123.5k');
+  });
+});
+
+describe("Rating.vue", () => {
+  const wrapper = shallowMount(Rating, {
+    propsData: {
+      rating: "5.0",
+      ratings_count: 999
+    },
+    stubs: ["b-icon-star-fill", "b-icon-star-half", "b-icon-star"]
+  });
+
+  it("tests for rating smart_count = 999", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.vm.ratings_count).toBe(999);
+    expect(wrapper.vm.full_stars).toBe(5);
+    expect(wrapper.vm.has_half_star).toBe(0);
+    expect(wrapper.vm.empty_stars).toBe(0);
+    expect(wrapper.vm.smart_number).toBe('1k');
+  });
+});
+
+describe("Rating.vue", () => {
+  const wrapper = shallowMount(Rating, {
+    propsData: {
+      rating: "5.0",
+      ratings_count: 1000
+    },
+    stubs: ["b-icon-star-fill", "b-icon-star-half", "b-icon-star"]
+  });
+
+  it("tests for rating smart_count = 1000", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.vm.ratings_count).toBe(1000);
+    expect(wrapper.vm.full_stars).toBe(5);
+    expect(wrapper.vm.has_half_star).toBe(0);
+    expect(wrapper.vm.empty_stars).toBe(0);
+    expect(wrapper.vm.smart_number).toBe('1k');
+  });
+});
