74f6454953609dc6bb11f8a714c00a42321cb283
Otto <otto@masterbranch.io>
2020-06-02 16:39:37 +0100

    removing unused method

--- src/utils/formatters/date_formatters/distance_calculator.js
+++ src/utils/formatters/date_formatters/distance_calculator.js
@@ -3,10 +3,6 @@ export function days_since_monday(date){
     return (day_number === 0 ? 7 : day_number % 7) - 1;
 }
 
-export function days_til_sunday(date){
-    return 7 - date.getDay();
-}
-
 export function days_between_dates(date_1, date_2){
     let diff = date_2.getTime() - date_1.getTime();
     let diff_in_days = Math.floor(diff / (1000 * 3600 * 24));
